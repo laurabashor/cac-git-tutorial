16a8f9bb3077a71596787e58d83bdc51dcf36be6
laurabashor <laurabashor@mail.com>
2022-04-19 10:27:41 -0600

    finished config

--- git_config_instructions.R
+++ git_config_instructions.R
@@ -8,9 +8,13 @@ usethis::create_github_token()
 ## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
 
 ## set personal access token:
-credentials::set_github_pat("YourPAT")
+credentials::set_github_pat("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")
 
 ## or store it manually in '.Renviron':
-usethis::edit_r_environ()
+# usethis::edit_r_environ()
 ## store your personal access token with: GITHUB_PAT=xxxyyyzzz
 ## and make sure '.Renviron' ends with a newline
+
+#restart R!
+
+usethis::git_sitrep()
