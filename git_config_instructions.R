e220a1b7f6af0fb8b89cb2a082130089a260a084
Laura Bashor <laura.bashor@colostate.edu>
2022-04-19 10:19:16 -0600

    first commit

---
+++ git_config_instructions.R
@@ -0,0 +1,16 @@
+### 3. Configure git with Rstudio ############################################
+
+## set your user name and email:
+usethis::use_git_config(user.name = "laurabashor", user.email = "laurabashor@mail.com")
+
+## create a personal access token for authentication:
+usethis::create_github_token() 
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## set personal access token:
+credentials::set_github_pat("YourPAT")
+
+## or store it manually in '.Renviron':
+usethis::edit_r_environ()
+## store your personal access token with: GITHUB_PAT=xxxyyyzzz
+## and make sure '.Renviron' ends with a newline
